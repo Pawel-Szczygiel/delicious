84e16508bdfe9bfb8fd620e61aa1c9ac8410aeda
Paweł Szczygieł <pawel86szczygiel@gmail.com>
2023-10-21 18:57:48 +0200

    starting... procject from 0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+        <h1>hello  </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        {process.env.REACT_APP_API_KEY}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <pre>{process.env.REACT_APP_API_KEY}</pre>
     </div>
   );
 }
