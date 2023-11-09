8f8408cca426222b1ca7a4f81b59f4d52ea8c4c6
Paweł Szczygieł <pawel86szczygiel@gmail.com>
2023-11-09 22:43:02 +0100

    display info and igredients of any

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -6,6 +6,7 @@ import axios from "axios";
 const Recipe = () => {
   const { id } = useParams();
   const [recipeInfo, setRecipeInfo] = useState({});
+  const [activeBtn, setActiveBtn] = useState("Ingredients");
 
   const fetchDetails = async (id) => {
     try {
@@ -26,87 +27,124 @@ const Recipe = () => {
 
   return (
     <RecipeInfoWrapper>
-      <div>
+      <BoxOne>
         <h2>{recipeInfo.title}</h2>
         <img src={recipeInfo.image} alt={recipeInfo.title} />
-      </div>
-      <Info>
-        <Button>Instructions</Button>
-        <Button>Ingredients</Button>
-    
-      </Info>
+      </BoxOne>
+      <BoxTwo>
+        <Button
+          className={activeBtn === "Instructions" ? "active" : ""}
+          onClick={() => setActiveBtn("Instructions")}
+        >
+          Instructions
+        </Button>
+        <Button
+          className={activeBtn === "Ingredients" ? "active" : ""}
+          onClick={() => setActiveBtn("Ingredients")}
+        >
+          Ingredients
+        </Button>
+        {activeBtn === "Instructions" && (
+          <Info>
+            <h3>Summary</h3>
+            <p dangerouslySetInnerHTML={{ __html: recipeInfo.summary }}></p>
+            <h3>Instructions</h3>
+            <section
+              dangerouslySetInnerHTML={{ __html: recipeInfo.instructions }}
+            ></section>
+          </Info>
+        )}
+        {activeBtn === "Ingredients" && (
+          <>
+            <h3>Extended Ingredients</h3>
+            <ul>
+              {recipeInfo?.extendedIngredients?.map((ingredient) => {
+                return <li key={ingredient.id}>{ingredient.original}</li>;
+              })}
+            </ul>
+          </>
+        )}
+      </BoxTwo>
     </RecipeInfoWrapper>
   );
 };
 
 const RecipeInfoWrapper = styled.div`
-  margin: 4rem 0 4rem 0;
-  padding: 5rem 0;
+  margin: 2rem auto;
+  padding: 2rem 0;
   display: flex;
   justify-content: space-evenly;
-
-  background: rgba(255, 255, 255, 0.1);  
+  align-items: center;
+  flex-wrap: wrap;
+  min-width: 700px;
+  max-width: 1200px;
+  color: #313131;
+  background: rgba(255, 255, 255, 0.1);
   box-shadow: rgb(38, 57, 77) 0px 20px 20px -10px;
   backdrop-filter: blur(20px);
   -webkit-backdrop-filter: blur(20px);
   border-radius: 10px;
-  border: 1px solid rgba(255, 255, 255, 0.18);
+  border: 1px solid rgba(255, 255, 255, 0.4);
 
   .active {
-    background: linear-gradient(35deg, #494949, #313131);
-    color: #fff;
-  }
-
-  @media (max-width: 1440px) {
-    & {
-      flex-direction: column;
-      align-items: center;
-      justify-content: space-between;
-      gap: 4rem;
-    }
-  }
-
-  h2 {
-    margin-bottom: 2rem;
-    letter-spacing: 0.1rem;
-  }
-  img {
-    min-width: 50%;
-  }
-
-  ul {
-    margin-top: 2rem;
-  }
-
-  li {
-    font-size: 1.2rem;
-    line-height: 2.5rem;
+    border: 2px solid rgba(0, 0, 0, 0.7);
   }
 `;
 
 const Button = styled.button`
   padding: 1rem 2rem;
   color: #313131;
-  background: rgba(0, 0, 0, .01);
+  background: rgba(0, 0, 0, 0.01);
   border: 1px solid rgba(0, 0, 0, 0.1);
   backdrop-filter: blur(20px);
   -webkit-backdrop-filter: blur(20px);
   border-radius: 1rem;
   margin: 1rem;
-  letter-spacing: .2rem;
+  letter-spacing: 0.2rem;
   font-weight: 600;
+  font-family: "Montserrat", -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+  'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+  sans-serif;
   cursor: pointer;
-  transition: box-shadow .3s linear;
+  transition: box-shadow 0.3s linear;
 
   &:hover {
     box-shadow: rgb(38, 57, 77) 0px 5px 15px -8px;
   }
 `;
 
-const Info = styled.div`
-  min-width: 50%;
-  text-align: left;
-  padding: 0 2rem;
+const BoxOne = styled.div`
+  width: 500px;
+  padding: 1rem;
+  align-self: flex-start;
+
+  @media (max-width: 1269px) {
+    & {
+      width: 90%;
+    }
+  }
+  h2 {
+    margin-bottom: 1rem;
+    letter-spacing: 0.1rem;
+  }
+
+  img {
+    width: 100%;
+    height: 300px;
+    object-fit: cover;
+    border-radius: 2rem;
+    transition: all 0.3s linear;
+    user-select: none;
+    box-shadow: rgb(38, 57, 77) 0 15px 10px -10px;
+  }
+  img:hover {
+    box-shadow: rgb(38, 57, 77) 0 17px 14px -9px;
+  }
+`;
+
+const BoxTwo = styled.div`
+  width: 500px;
+  padding: 1rem;
   background: rgba(255, 255, 255, 0.1);
   box-shadow: rgb(38, 57, 77) 0px 20px 20px -10px;
   backdrop-filter: blur(20px);
@@ -114,10 +152,45 @@ const Info = styled.div`
   border-radius: 10px;
   border: 1px solid rgba(255, 255, 255, 0.18);
 
-  @media (max-width: 1440px) {
-    & button {
-      text-align: center;
+  @media (max-width: 1269px) {
+    & {
+      width: 90%;
+      margin-top: 1rem;
     }
+  }
+
+  h3 {
+    margin-top: 1rem;
+  }
+
+  p {
+    padding: 1rem 0;
+    line-height: 1.5;
+    font-size: 1.1rem
+  }
+
+  ul {
+    margin-top: 2rem;
+  }
+
+  li {
+    list-style-type: none;
+    font-size: 1.1rem;
+    line-height: 2.5rem;
+  }
+`;
+
+const Info = styled.article`
+  width: 100%;
+  padding: 1rem;
+
+  section {
+    line-height: 1.5;
+    font-size: 1.1rem;
+    padding-bottom: 1rem;
+  }
+  ::selection {
+    background-color: lightgreen;
   }
 `;
 
